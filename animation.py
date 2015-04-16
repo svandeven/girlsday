05d550c3a650ddb14113dc43d40c950a4ee084a9
B <bkiera@gmail.com>
2015-04-16 09:58:04 +0200

    Simplified the language

--- animation.py
+++ animation.py
@@ -10,86 +10,35 @@ from sun import *
 
 
 i = 0
-while i < 1000:
-
+while i < 10:
+    i = i + 1
     show(minion)
-    sleep(0.1)
-
     show(minion2)
-    sleep(0.1)
-
     show(minion)
-    sleep(0.1)
-
     show(minion3)
-    sleep(0.1)
-    i = i + 100
 
 i = 0
 while i < 5:
     i = i + 1
     
     show(sun1)
-    sleep(0.1)
-
     show(sun2)
-    sleep(0.1)
-    
-    show(sun3)
-    sleep(0.1)
-    
+    show(sun3)    
     moveDown(sun3, 1)
-    sleep(0.1)
-
     moveDown(sun3, 2)
-    sleep(0.1)
-
     moveDown(sun3, 3)
-    sleep(0.1)
-
     moveUp(sun1, 1)
-    sleep(0.1)
-
     moveUp(sun2, 2)
-    sleep(0.1)
-
     moveUp(sun3, 3)
-    sleep(0.1)
-
     moveRight(sun1, 10)
-    sleep(0.1)
-
     moveRight(sun2, 20)
-    sleep(0.1)
-
     moveRight(sun3, 30)
-    sleep(0.1)  
-    
-    moveRight(sun3, 40)
-    sleep(0.1)  
-    
+    moveRight(sun2, 40)
     moveRight(sun3, 50)
-    sleep(0.1)
-    
-    moveRight(sun3, 40)
-    sleep(0.1)
-    
+    moveRight(sun2, 40)
     moveRight(sun3, 30)
-    sleep(0.1)
-    
     moveRight(sun2, 20)
-    sleep(0.1)
-    
     moveRight(sun1, 10)
-    sleep(0.1)
-    
     moveLeft(sun1, 1)
-    sleep(0.1)
-
     moveLeft(sun2, 2)
-    sleep(0.1)
-    
     moveLeft(sun3, 3)
-    sleep(0.1)
-    
-
--- girlsday.py
+++ girlsday.py
@@ -4,6 +4,8 @@
 import subprocess as sp
 import time
 
+sleepTime = 0.2
+
 def clear():
     sp.call('clear',shell=True)
 def sleep(sec):
@@ -12,12 +14,14 @@ def sleep(sec):
 def show(frame):
     clear()
     print frame
+    sleep(sleepTime)
     
 def moveDown(frame, delta):
     clear()
     for l in range (0, delta):
         print ""
     print frame
+    sleep(sleepTime)
     
 def moveUp(frame, delta):
     clear()
@@ -26,6 +30,7 @@ def moveUp(frame, delta):
         if i > delta:
             print l
         i += 1
+    sleep(sleepTime)
         
 def moveRight(frame, delta):
     clear()
@@ -34,8 +39,10 @@ def moveRight(frame, delta):
         prefix += " "
     for l in frame.splitlines(): 
         print prefix + l
+    sleep(sleepTime)
         
 def moveLeft(frame, delta):
     clear()
     for l in frame.splitlines(): 
         print l[delta-1:]
+    sleep(sleepTime)        
